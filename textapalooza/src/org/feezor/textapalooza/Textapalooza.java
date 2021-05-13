662ed0928ec1e1de96a8ca4388c5ca3c73f6edac
YoreDad <feezjaglg@gmail.com>
2021-05-13 18:48:48 -0400

    switch to awt from swt.  include libs

--- textapalooza/src/org/feezor/textapalooza/Textapalooza.java
+++ textapalooza/src/org/feezor/textapalooza/Textapalooza.java
@@ -1,10 +1,14 @@
 package org.feezor.textapalooza;
 
+import org.feezor.textapalooza.game.GameWindow;
+
 public class Textapalooza {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("Starting textapalooza...");
+		GameWindow gw = new GameWindow();
+		gw.main(args);
 	}
 
 }
--- textapalooza/src/org/feezor/textapalooza/game/GameWindow.java
+++ textapalooza/src/org/feezor/textapalooza/game/GameWindow.java
@@ -1,80 +1,72 @@
 package org.feezor.textapalooza.game;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.wb.swt.SWTResourceManager;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class GameWindow {
 
-	protected Shell shell;
-	private Text text;
-	private Text txtHello;
-
+	private JFrame frame;
+	private JTextField textField;
+	private JTextArea textArea;
+	private JButton submitBtn;
+	private JLabel lblNewLabel;
+	
 	/**
 	 * Launch the application.
-	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
-			GameWindow window = new GameWindow();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		Display display = Display.getDefault();
-		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameWindow window = new GameWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		}
+		});
 	}
 
 	/**
-	 * Create contents of the window.
+	 * Create the application.
 	 */
-	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(934, 550);
-		shell.setText("SWT Application");
-		shell.setLayout(null);
-		
-		text = new Text(shell, SWT.BORDER);
-		text.setEditable(false);
-		text.setBounds(10, 478, 819, 21);
-		
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.setBounds(851, 476, 57, 25);
-		btnNewButton.setText("Submit");
-		
-		ScrolledComposite scrolledComposite = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		scrolledComposite.setBounds(10, 42, 898, 428);
-		scrolledComposite.setExpandHorizontal(true);
-		scrolledComposite.setExpandVertical(true);
-		
-		txtHello = new Text(scrolledComposite, SWT.BORDER);
-		txtHello.setText("Hello");
-		scrolledComposite.setContent(txtHello);
-		scrolledComposite.setMinSize(txtHello.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-		
-		Label lblNewLabel = new Label(shell, SWT.NONE);
-		lblNewLabel.setAlignment(SWT.CENTER);
-		lblNewLabel.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
-		lblNewLabel.setBounds(10, 10, 898, 21);
-		lblNewLabel.setText("Textapalooza V1.0");
+	public GameWindow() {
+		initialize();
+	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 986, 652);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(40, 60, 869, 463);
+		frame.getContentPane().add(scrollPane);
+		
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
+		textField = new JTextField();
+		textField.setBounds(22, 557, 646, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		submitBtn = new JButton("Submit");
+		submitBtn.setBounds(678, 554, 105, 32);
+		frame.getContentPane().add(submitBtn);
+		
+		lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(40, 11, 862, 38);
+		frame.getContentPane().add(lblNewLabel);
 	}
 }
